04e8e0da1b8eafc474aa2e246e9e41b7529ca424
Rodrigo Fuenzalida <rfuenzalidac87@gmail.com>
2020-03-30 21:49:44 -0300

    add program

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func main() {
+	postgresDSN := os.Getenv("POSTGRES_DSN")
+
+	for {
+		db, err := NewPostgres(postgresDSN)
+		if err != nil {
+			time.Sleep(1 * time.Second)
+			continue
+		}
+		db.Close()
+		break
+	}
+
+	fmt.Print("Up")
+
+	os.Exit(0)
+}
+
+// NewPostgres creates a new Database with postgres as driver.
+func NewPostgres(dsn string) (*sqlx.DB, error) {
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
